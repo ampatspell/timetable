ebf62bdc7467bbe8c24c9ed0d4a82ff99cb4cc84
Arnis Vuskans <ampatspell@gmail.com>
2026-02-20 01:31:18 +0200

    Update draw.rs

--- ui/src/draw.rs
+++ ui/src/draw.rs
@@ -3,11 +3,15 @@ use embedded_graphics::{
     mono_font::{MonoTextStyle, ascii::FONT_9X18_BOLD},
     pixelcolor::Rgb565,
     prelude::{Dimensions, DrawTarget, Point, RgbColor},
+    primitives::{PrimitiveStyle, StyledDrawable},
     text::Text,
 };
 use no_std_strings::str32;
 
 use crate::payload::Payload;
+
+const WIDTH: i32 = 280;
+const HEIGHT: i32 = 240;
 
 pub fn draw_content<D>(display: &mut D, payload: Payload) -> ()
 where
@@ -19,8 +23,16 @@ where
     st.push_str("C");
 
     let position = Point::new(40, 30);
-    let style = MonoTextStyle::new(&FONT_9X18_BOLD, Rgb565::WHITE);
+    let style = MonoTextStyle::new(&FONT_9X18_BOLD, Rgb565::new(5, 11, 5));
+    let stroke = PrimitiveStyle::with_stroke(Rgb565::RED, 1);
 
-    display.clear(Rgb565::RED).ok();
-    Text::new(&st, position, style).draw(display).ok();
+    display.clear(Rgb565::new(21, 62, 0)).ok();
+
+    let text = Text::new(&st, position, style);
+    let bb = text.bounding_box();
+    // let x: i32 = (WIDTH / 2) + (bb.width / 2);
+    // let y: i32 = (HEIGHT / 2) + (bb.height / 2);
+    // text.position = Point::new(x, y);
+    text.draw(display).ok();
+    bb.draw_styled(&stroke, display).ok();
 }
