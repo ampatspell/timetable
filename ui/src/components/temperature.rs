703b6938e4c6539c697490b279ca409962c23416
Arnis Vuskans <ampatspell@gmail.com>
2026-02-20 23:31:25 +0200

    draaaw

--- ui/src/components/temperature.rs
+++ ui/src/components/temperature.rs
@@ -1,10 +1,17 @@
 use core::ops::Add;
 
-use embedded_graphics::{pixelcolor::Rgb565, prelude::*};
+use embedded_graphics::{
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{Circle, PrimitiveStyle},
+};
 use profont::PROFONT_18_POINT;
 
 use crate::{
-    components::utils::{TextOptions, draw_text, float_to_string},
+    components::{
+        TEXT_COLOR,
+        utils::{TextOptions, draw_text, float_to_string, sign32},
+    },
     payload::Temperature,
 };
 
@@ -22,27 +29,49 @@ where
         origin,
     } = opts;
 
-    let string = float_to_string(temperature.value);
     {
-        let mut string = string;
-        string.push_str(" C");
+        // -19.2 C
+        let string = float_to_string(temperature.value);
+        let string = {
+            let mut string = string;
+            string.push(" C");
+            string
+        };
+
+        let font = PROFONT_18_POINT;
+        let bounding_box = {
+            let bounding_box = draw_text(
+                display,
+                TextOptions {
+                    origin: origin.add(Point::new(0, 0)),
+                    string: &string,
+                    font: &font,
+                },
+            );
+
+            bounding_box
+        };
+
+        // °
+        {
+            let width: i32 = sign32(bounding_box.size.width);
+            let height: i32 = sign32(bounding_box.size.height);
+            let origin = Point::new(bounding_box.top_left.x + width + 2, origin.y - height + 6);
+            Circle::new(origin, 7)
+                .into_styled(PrimitiveStyle::with_stroke(TEXT_COLOR, 2))
+                .draw(display)
+                .ok();
+        }
     }
 
-    draw_text(
-        display,
-        TextOptions {
-            origin: origin.add(Point::new(0, 0)),
-            string: &string,
-            font: &PROFONT_18_POINT,
-        },
-    );
-
-    draw_text(
-        display,
-        TextOptions {
-            origin: origin.add(Point::new(0, 15)),
-            string: &temperature.description,
-            font: &profont::PROFONT_14_POINT,
-        },
-    );
+    {
+        draw_text(
+            display,
+            TextOptions {
+                origin: origin.add(Point::new(0, 15)),
+                string: &temperature.description,
+                font: &profont::PROFONT_14_POINT,
+            },
+        );
+    }
 }
--- ui/src/components/utils.rs
+++ ui/src/components/utils.rs
@@ -2,6 +2,7 @@ use embedded_graphics::{
     mono_font::{MonoFont, MonoTextStyleBuilder},
     pixelcolor::Rgb565,
     prelude::*,
+    primitives::Rectangle,
     text::Text,
 };
 use no_std_strings::str32;
@@ -14,7 +15,7 @@ pub struct TextOptions<'a> {
     pub font: &'a MonoFont<'a>,
 }
 
-pub fn draw_text<D>(display: &mut D, opts: TextOptions) -> ()
+pub fn draw_text<D>(display: &mut D, opts: TextOptions) -> Rectangle
 where
     D: DrawTarget<Color = Rgb565> + Dimensions,
 {
@@ -25,10 +26,17 @@ where
         .build();
     let text = Text::new(opts.string, opts.origin, style);
     text.draw(display).ok();
+    let bounding_box = text.bounding_box();
+
+    bounding_box
 }
 
 pub fn float_to_string(value: f32) -> str32 {
     let mut buffer = ryu::Buffer::new();
     let value = buffer.format(value);
     str32::from(value)
+}
+
+pub fn sign32(value: u32) -> i32 {
+    value.try_into().unwrap()
 }
