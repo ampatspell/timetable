8d03261d8a05e83f561971651f7168f91d0a1506
Arnis Vuskans <ampatspell@gmail.com>
2026-02-22 18:54:37 +0200

    ugh

--- ui/src/draw.rs
+++ ui/src/draw.rs
@@ -8,9 +8,8 @@ use embedded_graphics::prelude::*;
 // const WIDTH: i32 = 280;
 // const HEIGHT: i32 = 240;
 
-pub fn draw_content(display: &mut impl Display, payload: Payload) -> () {
-    let icons = Icons::new();
-    let blocks = Blocks::new(Point::new(10, 10), &icons);
+pub fn draw_content<'a>(display: &mut impl Display, payload: Payload, icons: &'a Icons<'a>) -> () {
+    let blocks = Blocks::new(Point::new(10, 10), icons);
     blocks.draw(display);
 }
 
