d2f4ce3a89335dfad294443e3a6b9203fc1c848e
Arnis Vuskans <ampatspell@gmail.com>
2026-02-25 01:21:21 +0200

    take a screenshot

--- font/src/main.rs
+++ font/src/main.rs
@@ -1,34 +1,34 @@
-use std::fs::{read_to_string, write};
+use std::{ffi::OsStr, fs::write};
 
 use headless_chrome::{
-    Browser,
-    protocol::cdp::{Page::CaptureScreenshotFormatOption::Png, Target::CreateTarget},
+    Browser, LaunchOptions, protocol::cdp::Page::CaptureScreenshotFormatOption::Png,
 };
 
 fn main() {
-    let content = read_to_string("src/template.html").expect("Template");
-    let script = format!("document.body.innerHTML = `{content}`");
+    let font_size = 20;
 
-    let browser = Browser::default().unwrap();
-    let tab = browser
-        .new_tab_with_options(CreateTarget {
-            url: String::from("about:blank"),
-            left: None,
-            top: None,
-            width: Some(1024),
-            height: Some(300),
-            window_state: None,
-            browser_context_id: None,
-            enable_begin_frame_control: None,
-            new_window: Some(true),
-            background: None,
-            for_tab: None,
-            hidden: Some(false),
-        })
-        .unwrap();
+    let args = vec![OsStr::new(
+        "--disable-features=HttpsUpgrades,HttpsFirstBalancedModeAutoEnable",
+    )];
 
-    tab.evaluate(&script, false).unwrap();
-    tab.wait_for_element(".done").unwrap();
+    let browser = Browser::new(
+        LaunchOptions::default_builder()
+            .headless(false)
+            .args(args)
+            .window_size(Some((1024, 600)))
+            .build()
+            .unwrap(),
+    )
+    .unwrap();
+
+    let tab = browser.new_tab().unwrap();
+    tab.navigate_to(
+        format!("http://timetable.app.amateurinmotion.com/font?font-size={font_size}").as_str(),
+    )
+    .unwrap()
+    .wait_for_element(".done")
+    .unwrap();
+
     let screenshot = tab.capture_screenshot(Png, None, None, true).unwrap();
 
     write("screenshot.png", screenshot).unwrap();
