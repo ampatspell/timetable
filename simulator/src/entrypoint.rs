a2b3f70d767f98c289cc2b628838b6cd55915331
Arnis Vuskans <ampatspell@gmail.com>
2026-02-27 02:11:29 +0200

    Update entrypoint.rs

--- simulator/src/entrypoint.rs
+++ simulator/src/entrypoint.rs
@@ -1,12 +1,13 @@
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics_simulator::{SimulatorDisplay, SimulatorEvent, Window};
+use no_std_strings::str12;
 use ui::ui::UI;
 
 pub fn main_loop(display: &mut SimulatorDisplay<Rgb565>, window: &mut Window) -> () {
     let mut ui = UI::new();
     ui.prepare(display);
     loop {
-        ui.update();
+        ui.on_time(display, str12::from("00:00:00"));
         ui.draw(display);
         window.update(&display);
 
